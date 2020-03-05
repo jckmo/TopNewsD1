68d16823dffe42ed1d17ac4df717d62a43dc68ff
ohjacksn <jacksonmo@me.com>
2020-03-05 12:58:30 -0500

    Create CLI Controller

---
+++ lib/TopNewsD1.rb
@@ -0,0 +1,6 @@
+require "TopNewsD1/version"
+
+module TopNewsD1
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/TopNewsD1_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TopNewsD1 do
+  it "has a version number" do
+    expect(TopNewsD1::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
